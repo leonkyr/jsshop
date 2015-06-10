a55e32b6980d612aee023a7593339fe34fbd6d1a
Leonid Kyrpychenko <kyrpychenko@gmail.com>
2015-06-10 23:34:20 +0200

    fixed some typos

--- app/components/Breadcrumbs.js
+++ app/components/Breadcrumbs.js
@@ -1,19 +1,16 @@
 var React = require("react");
-var Router = require("react-router");
-var Link = Router.Link;
-var mui = require("material-ui");
-var ImmutableRenderMixin = require("react-immutable-render-mixin");
 
 var Breadcrumbs = React.createClass({
   render: function() {
-    var crumbs = [];
-
+    var crumbs = [
+      <li>Home</li>
+    ];
     return (
       <ul className="breadcrumbs">
         {crumbs}
       </ul>
     );
-  };
+  }
 });
 
 module.exports = Breadcrumbs;
